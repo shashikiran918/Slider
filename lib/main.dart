619edef28d2e3587adb807fbfeed626b00f851e5
shashikiran918 <85386298+shashikiran918@users.noreply.github.com>
2021-06-07 19:33:51 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MySliderApp(),
+    );
+  }
+}
+
+class MySliderApp extends StatefulWidget {
+
+  @override
+  _MySliderAppState createState() => _MySliderAppState();
+}
+
+class _MySliderAppState extends State<MySliderApp> {
+  int _value = 6;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Slider '),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Center(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Icon(
+                      Icons.volume_up,
+                      size: 40,
+                    ),
+                    new Expanded(
+                        child: Slider(
+                            value: _value.toDouble(),
+                            min: 1.0,
+                            max: 20.0,
+                            divisions: 10,
+                            activeColor: Colors.green,
+                            inactiveColor: Colors.orange,
+                            label: 'Set volume value',
+                            onChanged: (double newValue) {
+                              setState(() {
+                                _value = newValue.round();
+                              });
+                            },
+                        )
+                    ),
+                  ]
+              )
+          ),
+        )
+    );
+  }
+}
